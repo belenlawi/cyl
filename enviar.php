086db36385672999e74968a44d69040e1ad29f78
belenlawi <belen.lawi@gmail.com>
2020-02-19 00:06:34 -0300

    mi primer commit =)

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+$destino= "belen.lawi@gmail.com";
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
+$contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\ntelefono: " . $telefono . "\nMensaje: " . $mensaje;
+mail($destino,"Contacto", $contenido);
+header("Location:gracias.html");
+?>
+
